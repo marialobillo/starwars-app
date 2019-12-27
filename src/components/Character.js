eae13bb3bf194a164f4631444af3182fa1e75bc2
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-12-27 18:22:11 +0100

    request for homeworld and species

--- src/components/Character.js
+++ src/components/Character.js
@@ -2,25 +2,44 @@ import React, {Component} from 'react';
 
 
 class Character extends Component{
+    constructor(props){
+        super(props);
+        this.state = {
+            character: this.props.character,
+            homeworld: '',
+            species: '',
+        }
+    }
+    componentDidMount(){
+        this.getRequest('homeworld',this.state.character.homeworld);
+        this.getRequest('species', this.state.character.species);
+    }
+
+    getRequest = async (field, url) => {
+        
+        const request = await fetch(url);
+        const data = await request.json();
+
+        this.setState({
+            [field] : data.name,
+        })
+    }
 
     render(){
         const {
             name, 
             birth_year, 
             height, 
-            mass, 
-            homeworld, 
-            species
-        } = this.props.character;
-
+            mass
+        } = this.state.character;
         return (
         <tr className="">
             <td>{name}</td>
             <td>{birth_year}</td>
             <td>{height}</td>
             <td>{mass}</td>
-            <td>{homeworld}</td>
-            <td>{species}</td>
+            <td>{this.state.homeworld}</td>
+            <td>{this.state.species}</td>
         </tr>
         );
     }
