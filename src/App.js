05a27c2a74d75c73218cd34b211bb223933d047e
Maria Lobillo Santos <maria.lobillo.santos@gmail.com>
2019-12-28 19:32:50 +0100

    showing the 87 items without pages

--- src/App.js
+++ src/App.js
@@ -13,7 +13,10 @@ class App extends Component{
   }
   
   componentDidMount(){
-    this.getRequest();
+    for(let i = 1; i < 10; i++){
+      this.getRequest(i);
+    }
+    //this.getRequest();
     //this.setState({filteredCharacters: this.state.characters})
   }
     
@@ -23,17 +26,31 @@ class App extends Component{
     const request = await fetch(url);
     const people = await request.json();
     const results = people.results;
+    
+    const currentCharacters = [...this.state.characters];
+    console.log(page, results.length);
+    
+    results.forEach(item => {
+      if(!currentCharacters.includes(item.name)){
+        currentCharacters.push(item);
 
-    this.setState({
-      characters: results,
-      filteredCharacters: results,
+      }
     });
+  
+    console.log(currentCharacters);
+  
+    this.setState({
+      characters: currentCharacters,
+      filteredCharacters: currentCharacters,
+    });
+
   }
 
   handleChange = event => {
     let updatedList = this.state.characters;
     
     updatedList = updatedList.filter((item) => {
+
       const lowercase = item.name.toLowerCase();
       const filter = event.target.value.toLowerCase();
       return lowercase.includes(filter);
